eed12ac3734b0e3c5baa383ba1b3a455121f5e58
Tim <mythfish@gmail.com>
2019-07-16 13:11:42 +0800

    Bump version to 0.2.9

--- build_local.sh
+++ build_local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export RELEASEDIR=./release
-export VERSION=0.2.8
+export VERSION=0.2.9
 
 CGO_ENABLED=1 GOOS=linux  go build  -o $RELEASEDIR/build/usr/sbin/ovpmd ./cmd/ovpmd
 CGO_ENABLED=1 GOOS=linux  go build  -o $RELEASEDIR/build/usr/bin/ovpm   ./cmd/ovpm
